07f39fa72758cbdbd3be4df17c1fdb49f0f58d52
ptlergo <ptlergo@gmail.com>
2016-10-10 18:16:13 -0400

    app testing components render. success

---
+++ lab2/App/container/index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+import React, { Component } from 'react'
+import {
+  Stylesheet,
+  View,
+  Text
+} from 'react-native'
+
+// import custom components
+import Test from '../components/test';
+
+// created class
+export default class App extends Component {
+  render(){
+    return (
+      <Text>Hello</Text>
+    )
+  }
+}
