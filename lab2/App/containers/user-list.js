bfdbd4eda5cf4105f15b8e6ee2d399712805637f
ptlergo <ptlergo@gmail.com>
2016-10-10 21:13:55 -0400

    fixed function error in user-list

--- lab2/App/containers/user-list.js
+++ lab2/App/containers/user-list.js
@@ -9,18 +9,19 @@ import {
   Text
 } from 'react-native'
 
-export default class UserList extends Component {
+class UserList extends Component {
   render() {
     return (
       <Text>1</Text>
     )
   }
+}
 
-  // Send state from store into component as prop
-  function mapStateToProps(state) {
-    return {
-      users: state.users
-    }
-
+// Send state from store into component as prop
+function mapStateToProps(state) {
+  return {
+    users: state.users
   }
 }
+
+export default connect (mapStateToProps)(UserList);
