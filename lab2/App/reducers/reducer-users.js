0155c5cdf53107031d36593ce6394581480b021a
ptlergo <ptlergo@gmail.com>
2016-10-10 20:20:38 -0400

    static array for users reducer

---
+++ lab2/App/reducers/reducer-users.js
@@ -0,0 +1,30 @@
+'use strict'
+
+/**
+* function that returns static array of users
+**/
+export default function() {
+  return [
+    {
+      id: 0,
+      first: 'Patrick',
+      age: 26
+      description: 'college student'
+      thumbnail: 'http://i.imgur.com/j15Ee8f.jpg',
+    },
+    {
+      id: 1,
+      first: 'Sarah',
+      age: 21
+      description: 'college student'
+      thumbnail: 'http://i.imgur.com/Jvt3A24.jpg',
+    },
+    {
+      id: 2,
+      first: 'James',
+      age: 11
+      description: 'elementary student'
+      thumbnail: 'http://i.imgur.com/hwBdWCi.jpgx',
+    },
+  ]
+}
